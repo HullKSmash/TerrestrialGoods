bb173b9d9a078cbb14e948bf98add6aba1d3b751
Katie Hull <katie.ann.hull@gmail.com>
2020-03-31 17:00:22 -0700

    Updating deodorant products

--- src/app/deodorant/deodorant.component.ts
+++ src/app/deodorant/deodorant.component.ts
@@ -11,9 +11,38 @@ export class DeodorantComponent implements OnInit {
 
   constructor() {
     this.deodorantProducts = [
-      {category: "deodorant", brand: "Freewheelin' Market", type: "deodorant", vendor: "Etsy", title: "Natural Deodorant", description: "A natural deodorant in a plastic-free tube.  Available in multiple scents, including an unscented option.", image: "https://i.etsystatic.com/9676768/r/il/16d495/1673130514/il_794xN.1673130514_ghaj.jpg", link: "https://www.etsy.com/listing/647224207/natural-deodorant-in-eco-friendly-tube-3?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=deodorant+zero+waste&ref=sr_gallery-1-1&organic_search_click=1&pro=1"},
-      {category: "deodorant", brand: "Arcadia Natural", type: "deodorant", vendor: "Etsy", title: "Deodorant", description: "An all-natural, zero-waste deodorant in a paper-based tube.  Available in a wide array of scents, or unscented, for both men and women.", image: "https://i.etsystatic.com/8816097/r/il/d56765/1919350484/il_1588xN.1919350484_oseg.jpg", link: "https://www.etsy.com/listing/547221050/all-natural-zero-waste-deodorant-vegan?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=deodorant+zero+waste&ref=sc_gallery-1-1&plkey=909804b1ef291a863871ea8d5f4262dc58052dc8%3A547221050&frs=1&bes=1"},
-      {category: "deodorant", brand: "kleineAuszeit", type: "deodorant", vendor: "Etsy", title: "Deodorant", description: "A creme deodorant in a glass pot with a bamboo lid.  No plastic, aluminum, or palm oil.  Green tea scented.", image: "https://i.etsystatic.com/9818312/r/il/223c79/1708662711/il_1588xN.1708662711_rvx5.jpg", link: "https://www.etsy.com/listing/611576720/effective-deodorant-without-aluminum?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=deodorant+zero+waste&ref=sr_gallery-1-16&organic_search_click=1"}
+      {category: "deodorant", 
+        brand: "Ethique", 
+        type: "deodorant", 
+        vendor: "Amazon", 
+        title: "Eco-Friendly Deodorant Bar", 
+        description: "Deodorant in a familiar bar form, without the plastic case.  Available in two scent options or unscented.", 
+        image: "https://images-na.ssl-images-amazon.com/images/I/71vfODL7ZdL._SL1500_.jpg", 
+        link: "https://amzn.to/2UzZWL4"},
+      {category: "deodorant", 
+        brand: "Meow Meow Tweet", 
+        type: "deodorant", 
+        vendor: "Amazon", 
+        title: "Lavender or Tea Tree Deodorant Cream", 
+        description: "", 
+        image: "https://images-na.ssl-images-amazon.com/images/I/71vMKYvdRDL._AC_SL1098_.jpg", 
+        link: "https://amzn.to/2xFgkAC"},
+      {category: "deodorant", 
+        brand: "Meow Meow Tweet", 
+        type: "deodorant", 
+        vendor: "Amazon", 
+        title: "Baking Soda Free Sensitive Skin Lavender Deodorant Cream", 
+        description: "A gentle cream deodorant made without baking soda.  Vegan, organic, cruelty-free, fair trade, and plastic-free.", 
+        image: "https://images-na.ssl-images-amazon.com/images/I/41uQ0rslf%2BL.jpg", 
+        link:"https://amzn.to/2JqcAWB"},
+      {category: "deodorant", 
+        brand: "Primal Life Organics", 
+        type: "deodorant", 
+        vendor: "Amazon", 
+        title: "Stick Up Natural Vegan Deodorant", 
+        description: "All natural and friendly to sensitive skin.  Packaged in an eco-friendly cardboard tube.", 
+        image: "https://images-na.ssl-images-amazon.com/images/I/81%2Bz59c7urL._AC_SL1500_.jpg", 
+        link: "https://amzn.to/39xeGOX"}
     ]
    }
 
